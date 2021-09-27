3d85aae55b5810514323456dca2d21564845a7fb
semin-1432 <2007ksm@naver.com>
2021-09-27 14:45:54 +0900

    day7 ex03 commit

--- src/day7/ex03.java
+++ src/day7/ex03.java
@@ -1,5 +1,30 @@
 package day7;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ex03 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        try {
+            System.out.print("x값 : ");
+            int x= scan.nextInt();
+            System.out.print("y값 : ");
+            int y=scan.nextInt();
 
+            System.out.println("x*y=" + mul(x, y));
+            System.out.println("x/y=" + div(x, y));
+        }catch(InputMismatchException e){
+            //System.out.println(e.getMessage());
+            e.printStackTrace();
+        }catch(ArithmeticException e){
+            //System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally{System.out.println("프로그램을 종료합니다.");}
+    }
+    public static int mul(int x, int y){
+        return x*y;
+    }
+    public static int div(int x, int y){
+        return x/y;
+    }
 }
