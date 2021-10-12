3942bb100e95cbd539a3858773714beb0af78db7
semin-1432 <2007ksm@naver.com>
2021-10-12 21:31:31 +0900

    day10 ex1 done

--- src/day10/ex01.java
+++ src/day10/ex01.java
@@ -1,50 +1,50 @@
-package day10;
-//오답, 풀이,, 너무 어렵읍니다..
-
-import java.util.*;
-
-public class ex01 {
-    public static void main(String[] args) {
-        List<Person> list = new ArrayList<>();
-        list.add(new Person("Yoon", 23));
-        list.add(new Person("Hong", 53));
-        list.add(new Person("Kim", 37));
-        list.add(new Person("Park", 67));
-        list.add(new Person("Lee", 15));
-        Collections.sort(list);
-        Iterator<Person> itr=list.iterator();
-
-        System.out.println("*** 나이순 정렬 ***");
-        while(itr.hasNext())
-            System.out.println(itr.next().toString());
-        System.out.println();
-
-        Collections.sort(list, new PersonComparator());
-        itr=list.iterator();
-        System.out.println("*** 이름순 정렬 ***");
-        while(itr.hasNext())
-            System.out.println(itr.next().toString());
-        System.out.println();
-    }
-}
-class Person implements Comparable<Person> {
-    String name;
-    int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    public String toString() {
-        return name + " : " + age;
-    }
-    public int compareTo(Person p) {
-        return this.age-p.age;
-    }
-}
-
-class PersonComparator implements Comparator<Person>{
-    public int compare(Person p1, Person p2) {
-        return p1.name.compareTo(p2.name);
-    }
-}
+//package day10;
+////오답, 풀이,, 너무 어렵읍니다..
+//
+//import java.util.*;
+//
+//public class ex01 {
+//    public static void main(String[] args) {
+//        List<Person> list = new ArrayList<>();
+//        list.add(new Person("Yoon", 23));
+//        list.add(new Person("Hong", 53));
+//        list.add(new Person("Kim", 37));
+//        list.add(new Person("Park", 67));
+//        list.add(new Person("Lee", 15));
+//        Collections.sort(list);
+//        Iterator<Person> itr=list.iterator();
+//
+//        System.out.println("*** 나이순 정렬 ***");
+//        while(itr.hasNext())
+//            System.out.println(itr.next().toString());
+//        System.out.println();
+//
+//        Collections.sort(list, new PersonComparator());
+//        itr=list.iterator();
+//        System.out.println("*** 이름순 정렬 ***");
+//        while(itr.hasNext())
+//            System.out.println(itr.next().toString());
+//        System.out.println();
+//    }
+//}
+//class Person implements Comparable<Person> {
+//    String name;
+//    int age;
+//
+//    public Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//    public String toString() {
+//        return name + " : " + age;
+//    }
+//    public int compareTo(Person p) {
+//        return this.age-p.age;
+//    }
+//}
+//
+//class PersonComparator implements Comparator<Person>{
+//    public int compare(Person p1, Person p2) {
+//        return p1.name.compareTo(p2.name);
+//    }
+//}
--- src/day10/ex01_1.java
+++ src/day10/ex01_1.java
@@ -1,9 +1,6 @@
 package day10;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ex01_1 {
     public static void main(String[] args){
@@ -15,20 +12,22 @@ public class ex01_1 {
         list.add(new Person("Lee",15));
 
         Collections.sort(list);
-        Iterator<Person> itr=new Iterator<Person>() {
-            @Override
-            public boolean hasNext() {
-                return false;
-            }
+        Iterator<Person> itr=list.iterator();
 
-            @Override
-            public Person next() {
-                return null;
-            }
+        System.out.println("*** 나이순 정렬 ***");
+        while(itr.hasNext()){
+            System.out.println(itr.next().toString());
+        }
+
+        Collections.sort(list, new PComparator());
+        System.out.println("*** 이름순 정렬 ***");
+        itr=list.iterator(); //반복자 다시 획득
+        while(itr.hasNext()){
+            System.out.println(itr.next().toString());
         }
 ;    }
 }
-class Person{
+class Person implements Comparable<Person>{
     String name;
     int age;
 
@@ -36,4 +35,15 @@ class Person{
         this.name=name; this.age=age;
     }
     public String toString(){return name+":"+age;}
+
+    @Override
+    public int compareTo(Person o) {
+        //자신의 age가 o의 age보다 크다면 양수 반환, 같다면 0, 작으면 음수
+        return this.age-o.age;
+    }
+}
+class PComparator implements Comparator<Person> {
+    public int compare(Person o1, Person o2) {
+        return o1.name.compareTo(o2.name);
+    }
 }
